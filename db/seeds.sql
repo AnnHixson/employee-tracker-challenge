8d1ada91c1280dfd6db3d43825fcd26b4a7588f9
Ann <ann.hixson@uconn.edu>
2022-12-04 16:28:02 -0500

    added to schema

---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO department (name)
+VALUES
+    ("Food"),
+    ("Medicine"),
+    ("Entertainment"),
+    ("Navigation"),
+    ("Protection"),
+    ("Crafting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Chef", 177000000),
+    ("Navigator", 366000000),
+    ("Doctor", 100),
+    ("Captain", 3000000000),
+    ("Swordsman", 1111000000),
+    ("Sniper", 200000000),
+    ("Shipwright", 94000000),
+    ("Musician", 83000000),
+    ("Archaeologist", 930000000);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Luffy", "Monkey D."),
+    ("Zoro", "Roronoa"),
+    ("Nami"),
+    ("Usopp"),
+    ("Sanji", "Vinsmoke"),
+    ("Chopper", "Tony Tony"),
+    ("Robin", "Nico"),
+    ("Franky"),
+    ("Brook");
